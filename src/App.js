97a2b4ad06de83a5dfa9170e915aac8d11d44974
joerald <ngozika1105@gmail.com>
2024-02-03 10:18:48 +0100

    add routers to project

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import GameOfThrone from './movies/gameOfThrone/GameOfThrone';
 import './App.css';
 import { data } from './utils/data'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import Home from './movies/home/home';
 
 function App() {
+  const router = createBrowserRouter(createRoutesFromElements(
+    <>
+      <Route path='/' element={<Home/>}></Route>
+    <Route path='about' element={<GameOfThrone />} ></Route>
+    </>
+  ))
 
   return (
-    <div>
-      <GameOfThrone data ={data}/>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
